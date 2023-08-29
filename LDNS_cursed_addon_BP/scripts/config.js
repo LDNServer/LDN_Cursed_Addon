67b6b798b1bba03eb437a1440c3cd24030286499
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2023-08-29 02:01:12 +0000

    feat: ランダムなEffectを突然付与

---
+++ LDNS_cursed_addon_BP/scripts/config.js
@@ -0,0 +1,3 @@
+export const randomEffectInterval = 20; // ticks
+
+export const randomEffectRate = 100; // 1/n (default: 1/100)
--- LDNS_cursed_addon_BP/scripts/main.js
+++ LDNS_cursed_addon_BP/scripts/main.js
@@ -1,1 +1,16 @@
-import { world } from '@minecraft/server';
+// @ts-check
+
+import { system, world } from '@minecraft/server';
+import { MinecraftEffectTypes } from './lib/mojang-effect';
+import * as util from './util';
+import * as config from './config';
+
+system.runInterval(() => {
+  for (const player of world.getPlayers()) {
+    // nTickに1回 m分の1の確率でeffect付与
+    if (util.random(0, config.randomEffectRate - 1) === 0) {
+      const effectId = util.randomValue(Object.values(MinecraftEffectTypes));
+      player.addEffect(effectId, util.random(2, 30));
+    }
+  }
+}, config.randomEffectInterval);
