0ee9f5c09f4e5ef395be087ffc5bf51f4e68a737
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2023-10-21 14:46:17 +0000

    feat: ランダムに昼夜入れ替え

--- LDNS_cursed_addon_BP/scripts/config.js
+++ LDNS_cursed_addon_BP/scripts/config.js
@@ -1,5 +1,6 @@
 export const randomEffectInterval = 20*30; // ticks
-
 export const randomEffectRate = 25; // 1/n (default: 1/25)
-
 export const randomEffectDuration = [ 5, 30 ]; // [ min, max ] seconds
+
+export const skipNightRate = 100/13; // 100/n = n%
+export const skipDayRate = 100/6;
---
+++ LDNS_cursed_addon_BP/scripts/random_time.js
@@ -0,0 +1,28 @@
+import { TimeOfDay, system, world } from '@minecraft/server';
+import * as config from './config';
+import * as util from './util';
+
+let dayChecked = false;
+let nightChecked = false;
+system.runInterval(() => {
+  const time = world.getTimeOfDay();
+
+  // at day
+  if (!dayChecked && 0 < time && time < TimeOfDay.Night) {
+    nightChecked = false;
+    if (util.random(0, config.skipDayRate - 1) === 0) {
+      world.setTimeOfDay(TimeOfDay.Night);
+    } else {
+      dayChecked = true;
+    }
+  }
+
+  if (!nightChecked && TimeOfDay.Night < time) {
+    dayChecked = false;
+    if (util.random(0, config.skipNightRate - 1) === 0) {
+      world.setTimeOfDay(TimeOfDay.Sunrise);
+    } else {
+      nightChecked = true;
+    }
+  }
+}, 20);
