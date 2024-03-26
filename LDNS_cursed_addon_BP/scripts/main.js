acb020db796db644d336954e8ee1da22b6627033
Yon4800 <20A3007@sit.shonan-it.ac.jp>
2024-03-26 18:17:54 +0900

    いろいろ追加 - プレイヤー？追加    - プレイヤーの辺りにたまに出てくる    - 歩き回る    - 倒すとErrorインゴットか重い石が出てくる    - 何種類かある    - ネームタグはランダム - 村人？追加    - モンスター系を攻撃してくれる    - 出てから20秒後にデスポーンする    - スポーン条件はまだ - 変な鶏、顔だけ羊にモンスタータグを付与

--- LDNS_cursed_addon_BP/scripts/main.js
+++ LDNS_cursed_addon_BP/scripts/main.js
@@ -4,6 +4,7 @@ import { system, world } from '@minecraft/server';
 import './random_effect';
 import './dangerous_cactus';
 import './random_time';
+import './nametag_mysterious_players';
 
 const timezoneOffset = 0;
 
