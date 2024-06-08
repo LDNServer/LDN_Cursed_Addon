454a0761dcc96a3b160687abc29571dc43bd4238
Yon4800 <20A3007@sit.shonan-it.ac.jp>
2024-06-08 10:29:34 +0900

    20240608: 変更 ## Errorツール、防具を使ったときのダメージの条件を変更 - 装備を着たりメインハンドにものを持った時にダメージを受ける確率が増加 - ダメージのほかにも謎の音が流れたり、空腹になったりするように

--- LDNS_cursed_addon_BP/scripts/error-tool-damage.js
+++ LDNS_cursed_addon_BP/scripts/error-tool-damage.js
@@ -1,5 +1,6 @@
 import { EquipmentSlot, Player, system, world } from '@minecraft/server';
 import { random } from './util';
+import { MinecraftEffectTypes } from './lib/mojang-effect';
 
 const errorTools = [
     'ldns:error_sword',
@@ -22,15 +23,38 @@ system.runInterval(() => {
 function error_tool_damage(targetPlayer) {
     if (!(targetPlayer instanceof Player)) return;
     const equippable = targetPlayer.getComponent('minecraft:equippable');
-    if (
-        (
-            errorTools.includes(equippable.getEquipment(EquipmentSlot.Mainhand)?.typeId) ||
-            equippable.getEquipment(EquipmentSlot.Head)?.typeId === 'ldns:error_helmet' ||
-            equippable.getEquipment(EquipmentSlot.Chest)?.typeId === 'ldns:error_chestplate' ||
-            equippable.getEquipment(EquipmentSlot.Legs)?.typeId === 'ldns:error_leggings' ||
-            equippable.getEquipment(EquipmentSlot.Feet)?.typeId === 'ldns:error_boots'
-        ) && random(0, 666) === 78
+    let kr = 6;
+    let krx = 0;
+    if (errorTools.includes(equippable.getEquipment(EquipmentSlot.Mainhand)?.typeId)) {
+        krx += 1;
+    }
+    if (equippable.getEquipment(EquipmentSlot.Head)?.typeId === 'ldns:error_helmet') {
+        krx += 1;
+    }
+    if (equippable.getEquipment(EquipmentSlot.Chest)?.typeId === 'ldns:error_chestplate') {
+        krx += 1;
+    }
+    if (equippable.getEquipment(EquipmentSlot.Legs)?.typeId === 'ldns:error_leggings') {
+        krx += 1;
+    }
+    if (equippable.getEquipment(EquipmentSlot.Feet)?.typeId === 'ldns:error_boots') {
+        krx += 1;
+    }
+    if (errorTools.includes(equippable.getEquipment(EquipmentSlot.Mainhand)?.typeId) ||
+        equippable.getEquipment(EquipmentSlot.Head)?.typeId === 'ldns:error_helmet' ||
+        equippable.getEquipment(EquipmentSlot.Chest)?.typeId === 'ldns:error_chestplate' ||
+        equippable.getEquipment(EquipmentSlot.Legs)?.typeId === 'ldns:error_leggings' ||
+        equippable.getEquipment(EquipmentSlot.Feet)?.typeId === 'ldns:error_boots'
     ) {
-        targetPlayer.applyDamage(1);
+        let rand = Math.floor(random(0, (6666 / (kr * krx))));
+        if (rand === 78) {
+            targetPlayer.applyDamage(2);
+        }
+        if (rand === 66) {
+            targetPlayer.addEffect(MinecraftEffectTypes.Hunger, 10 * 20);
+        }
+        if (rand === 33) {
+            targetPlayer.playSound('ldns.ppyy_spawn')
+        }
     }
 }
