e352177bebc275396ce611898ddf537adfe90137
Yon4800 <20A3007@sit.shonan-it.ac.jp>
2024-06-09 20:19:17 +0900

    20240609_2: 変更 エラーツール類を持っているときの確率の値を変更(？)

--- LDNS_cursed_addon_BP/scripts/error-tool-damage.js
+++ LDNS_cursed_addon_BP/scripts/error-tool-damage.js
@@ -7,8 +7,7 @@ const errorTools = [
     'ldns:error_pickaxe',
     'ldns:error_axe',
     'ldns:error_shovel',
-    'ldns:error_hoe',
-    'ldns:error_ingot'
+    'ldns:error_hoe'
 ];
 
 system.runInterval(() => {
@@ -26,14 +25,16 @@ function error_tool_damage(targetPlayer) {
     if (!(targetPlayer instanceof Player)) return;
     const equippable = targetPlayer.getComponent('minecraft:equippable');
     const items = targetPlayer.runCommand('testfor @s[hasitem={item=ldns:pendant_of_twilight}]').successCount;
+    let errortoolscount = 0;
     let erroritemscount = 0;
     const { container } = targetPlayer.getComponent('minecraft:inventory');
     for (let i = 0; i < container.size; i++) {
         const item = container.getItem(i);
         if (!item) continue;
         errorTools.forEach((tool) => {
-            if (item.typeId === tool) erroritemscount += item.amount;
+            if (item.typeId === tool) errortoolscount += item.amount;
         })
+        if (item.typeId === 'ldns:error_ingot') erroritemscount += item.amount;
     }
     let krx = 0;
     if (equippable.getEquipment(EquipmentSlot.Head)?.typeId === 'ldns:error_helmet') krx += 4;
@@ -47,19 +48,19 @@ function error_tool_damage(targetPlayer) {
         equippable.getEquipment(EquipmentSlot.Feet)?.typeId === 'ldns:error_boots'
     ) {
         if (items <= 0) {
-            if (Math.floor(random(0, (6666 / (krx + (erroritemscount * 0.25))))) === 0) {
+            if (Math.floor(random(0, (6666 / ((krx + (errortoolscount * 2) + (erroritemscount * 0.25)))))) === 0) {
                 targetPlayer.applyDamage(2);
             }
-            if (Math.floor(random(0, (6666 / (krx + (erroritemscount * 0.25))))) === 0) {
+            if (Math.floor(random(0, (6666 / ((krx + (errortoolscount * 2) + (erroritemscount * 0.25)))))) === 0) {
                 targetPlayer.addEffect(MinecraftEffectTypes.Hunger, 10 * 20);
             }
-            if (Math.floor(random(0, (6666 / (krx + (erroritemscount * 0.25))))) === 0) {
+            if (Math.floor(random(0, (6666 / ((krx + (errortoolscount * 2) + (erroritemscount * 0.25)))))) === 0) {
                 targetPlayer.playSound('ldns.yy_spawn');
             }
-            if (Math.floor(random(0, (6666 / (krx + (erroritemscount * 0.25))))) === 0) {
+            if (Math.floor(random(0, (6666 / ((krx + (errortoolscount * 2) + (erroritemscount * 0.25)))))) === 0) {
                 targetPlayer.playSound('ldns.pp_spawn');
             }
-            if (Math.floor(random(0, (6666 / (krx + (erroritemscount * 0.25))))) === 0) {
+            if (Math.floor(random(0, (6666 / ((krx + (errortoolscount * 2) + (erroritemscount * 0.25)))))) === 0) {
                 targetPlayer.playSound('ldns.ppyy_spawn');
             }
         }
