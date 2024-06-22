4727c2331f5bfe26a3bd57216e2b702fadfbc619
Yon4800 <20A3007@sit.shonan-it.ac.jp>
2024-06-22 23:22:29 +0900

    20240622_4: 変更 - ErrorMobに殴られると低確率でインベントリ内のアイテムがごちゃごちゃになる   - アイテムを別のものにするのは廃止

--- LDNS_cursed_addon_BP/scripts/errormob-effect.js
+++ LDNS_cursed_addon_BP/scripts/errormob-effect.js
@@ -20,26 +20,11 @@ function errordamage(targetPlayer) {
     }
     // アイテムが置き換えられる
     else if (rand === 13) {
-        let randomitem = null;
-        const randitem = random(0, 4);
-        // 切り替えるアイテムを選択
-        if (randitem === 0) randomitem = new ItemStack("ldns:error_ingot", random(1, 6));
-        else if (randitem === 1) randomitem = new ItemStack("ldns:heavy_stone", random(1, 6));
-        else if (randitem === 2) randomitem = new ItemStack("ldns:ld5987", 1);
-        else if (randitem === 3) randomitem = new ItemStack("ldns:dn3895", 1);
-        else if (randitem === 4) randomitem = new ItemStack("minecraft:feather", random(1, 6));
         // インベントリ
         const { container } = targetPlayer.getComponent("inventory");
         for (let i = 0; i < container.size; i++) {
-            // コンテナが空の時
-            if (!container.getItem(i)) {
-                continue;
-            }
             // アイテムを置き換える
-            else {
-                let rand2 = random(0, 66);
-                if (rand2 === 6) container.setItem(i, randomitem);
-            }
+            container.swapItems(i, random(0, container.size - 1), container);
         }
     }
     // レベルリセット
