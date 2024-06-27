a590ca7aceba3afa7be8557b16bafcaba8515798
Yon4800 <20A3007@sit.shonan-it.ac.jp>
2024-06-27 17:28:41 +0900

    20240627_1: 仕様変更 - ランダムに時間が突然変更される仕様を変更 - 朝と夜の瞬間に時間が変化するのをいつでも朝か昼になるように変更

--- LDNS_cursed_addon_BP/scripts/config.js
+++ LDNS_cursed_addon_BP/scripts/config.js
@@ -1,6 +1,6 @@
 export const randomEffectInterval = 20*60; // ticks
-export const randomEffectRate = 75; // 1/n (default: 1/25)
+export const randomEffectRate = 75; // 1/n (default: 1/75)
 export const randomEffectDuration = [ 5, 30 ]; // [ min, max ] seconds
 
-export const skipNightRate = 100/13; // 100/n = n%
-export const skipDayRate = 100/6;
+export const skipNightRate = 12800;
+export const skipDayRate = 19200;
--- LDNS_cursed_addon_BP/scripts/random_time.js
+++ LDNS_cursed_addon_BP/scripts/random_time.js
@@ -2,29 +2,20 @@ import { TimeOfDay, system, world } from '@minecraft/server';
 import * as config from './config';
 import * as util from './util';
 
-let dayChecked = false;
-let nightChecked = false;
 system.runInterval(() => {
   const time = world.getTimeOfDay();
 
   // at day
-  if (!dayChecked && 0 < time && time < TimeOfDay.Night) {
-    nightChecked = false;
-    if (util.random(0, config.skipDayRate - 1) === 0) {
-      world.setTimeOfDay(TimeOfDay.Night);
-    } else {
-      dayChecked = true;
+  if (util.random(0, config.skipNightRate - 1) === 0) {
+    if (0 < time && time < TimeOfDay.Night) {
+      world.setTimeOfDay(TimeOfDay.Midnight);
     }
   }
-
-  if (!nightChecked && TimeOfDay.Night < time) {
-    dayChecked = false;
-    if (util.random(0, config.skipNightRate - 1) === 0) {
-      world.setTimeOfDay(TimeOfDay.Sunrise);
-    } else {
-      nightChecked = true;
+  if (util.random(0, config.skipDayRate - 1) === 0) {
+    if (TimeOfDay.Night < time) {
+      world.setTimeOfDay(TimeOfDay.Day);
     }
   }
-}, 20);
+}, 20 * 60);
 
 // 譎る俣繧貞､ｧ縺帙▽縺ｫ
