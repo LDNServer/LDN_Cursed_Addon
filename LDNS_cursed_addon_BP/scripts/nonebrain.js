c285105598a912e18ceaeba2320e2d551b3619fc
Yon4800 <20A3007@sit.shonan-it.ac.jp>
2024-10-08 21:54:40 +0900

    Test_20241008_2154: 割り当て ### 割り当て - メインブランチの割り当てをした

--- LDNS_cursed_addon_BP/scripts/nonebrain.js
+++ LDNS_cursed_addon_BP/scripts/nonebrain.js
@@ -1,6 +1,9 @@
-import { system, world } from "@minecraft/server";
+import { Player, system, world } from "@minecraft/server";
 import { random } from "./util";
 let noneint = 0;
+let nonebrainchat = String(random(10000000, 99999999));
+let random_message = ["NONE縺輔ｓ縺ｯ鄒弱＠縺�☆縺ｰ繧峨＠縺�〒縺�", "縺薙ｓ縺ｫ縺｡縺ｯ縲√♀荵�＠縺ｶ繧翫〒縺吶�NONE縺輔ｓ縲�", "NONE縺ｯ譛ｬ蠖薙↓蟄伜惠縺吶ｋ縺ｮ縺�繧阪≧縺�...��"];
+let random_message_int = random(0, random_message.length);
 world.afterEvents.dataDrivenEntityTrigger.subscribe(async (events) => {
     system.run(async () => {
         if (events.entity.typeId === "ldns:nonebrain") {
@@ -8,124 +11,166 @@ world.afterEvents.dataDrivenEntityTrigger.subscribe(async (events) => {
                 noneint++;
                 // ノンブラインがデスポーンした数が一定数を超えたとき
                 if (noneint >= 64) {
-                    // 様々なイベントが起こる
-                    world.getPlayers().forEach(async (v, i, a) => {
-                        v.playSound("ldns.beep");
-                        v.onScreenDisplay.setTitle("nonebrain");
-                        await system.waitTicks(20 * 4);
-                        const playerlocation = v.location;
-                        const kanasibari = system.runInterval(async () => {
-                            v.teleport(playerlocation);
-                            v.playSound("ldns.errormob_glitch", { volume: 0.1, pitch: 0.666 });
-                            const { container } = v.getComponent("inventory");
-                            for (let i = 0; i < container.size; i++) {
-                                container.swapItems(i, random(0, container.size - 1), container);
-                            }
-                            v.selectedSlotIndex = random(0, 9);
-                            await system.waitTicks(1);
-                        });
-                        const rands = random(0, 2);
-                        const randh = random(0, 4);
-                        switch (rands) {
-                            case 0:
-                                v.playSound("ldns.pp_spawn");
-                                v.playSound("ldns.beep");
-                                v.playSound("ldns.errormob_glitch");
-                                break;
-                            case 1:
-                                v.playSound("ldns.yy_spawn");
-                                v.playSound("ldns.beep");
-                                v.playSound("ldns.errormob_glitch");
-                                break;
-                        }
-                        switch (randh) {
-                            case 0:
-                                v.onScreenDisplay.setTitle("egn2");
-                                break;
-                            case 1:
-                                v.onScreenDisplay.setTitle("egn3");
-                                break;
-                            case 2:
-                                v.onScreenDisplay.setTitle("egn6");
-                                break;
-                            case 3:
-                                v.onScreenDisplay.setTitle("egn9");
-                                break;
-                        }
-                        await system.waitTicks(20 * 4);
-                        const randw = random(0, 2);
-                        v.playSound("ldns.errormob_errorwindow");
-                        switch (randw) {
-                            case 0:
-                                v.onScreenDisplay.setTitle("ew1");
-                                break;
-                            case 1:
-                                v.onScreenDisplay.setTitle("ew2");
-                                break;
-                        }
-                        await system.waitTicks(20 * 4);
-                        v.playSound("ldns.time_mad");
-                        console.info("We is cursed");
-                        for (let i = 0; i < 160; i++) {
-                            let titletextr = random(0, 6);
-                            switch (titletextr) {
-                                case 0:
-                                    v.onScreenDisplay.setTitle("あなたは呪われてない", { stayDuration: 2, fadeInDuration: 0, fadeOutDuration: 0, subtitle: String(random(0, 999999999)) });
-                                    break;
-                                case 1:
-                                    v.onScreenDisplay.setTitle("あなたは呪われている", { stayDuration: 2, fadeInDuration: 0, fadeOutDuration: 0, subtitle: String(random(0, 999999999)) });
-                                    break;
-                                case 2:
-                                    v.onScreenDisplay.setTitle("縺ゅ↑縺溷測繧上ｌ縺ｦ", { stayDuration: 2, fadeInDuration: 0, fadeOutDuration: 0, subtitle: String(random(0, 999999999)) });
-                                    break;
-                                case 3:
-                                    v.onScreenDisplay.setTitle("縺ゅ↑縺溷測繧上ｌ縺ｾ", { stayDuration: 2, fadeInDuration: 0, fadeOutDuration: 0, subtitle: String(random(0, 999999999)) });
-                                    break;
-                                case 4:
-                                    v.onScreenDisplay.setTitle("We is Cursed.", { stayDuration: 2, fadeInDuration: 0, fadeOutDuration: 0, subtitle: String(random(0, 999999999)) });
-                                    break;
-                                case 5:
-                                    v.onScreenDisplay.setTitle("They am Cursed.", { stayDuration: 2, fadeInDuration: 0, fadeOutDuration: 0, subtitle: String(random(0, 999999999)) });
-                                    break;
-                            }
-                            world.setTimeOfDay(random(1, 23999));
-                            await system.waitTicks(1);
-                        }
-                        const rande = random(0, 4);
-                        if (rande === 0) {
-                            v.playSound("ldns.beep");
-                            v.onScreenDisplay.setTitle("Error1");
-                        }
-                        else if (rande === 1) {
-                            v.playSound("ldns.beep");
-                            v.onScreenDisplay.setTitle("Error2");
-                        }
-                        else if (rande === 2) {
-                            v.playSound("ldns.beep");
-                            v.onScreenDisplay.setTitle("Cursed1");
-                        }
-                        else if (rande === 3) {
-                            v.playSound("ldns.beep");
-                            v.onScreenDisplay.setTitle("Cursed2");
-                        }
-                        await system.waitTicks(20 * 4);
-                        v.playSound("ldns.pp_spawn");
-                        v.playSound("ldns.yy_spawn");
-                        v.playSound("ldns.ppyy_spawn");
-                        v.onScreenDisplay.setTitle("ppse");
-                        await system.waitTicks(20 * 6);
-                        system.clearRun(kanasibari);
-                        for (i = 0; i < 120; i++) {
-                            v.teleport(playerlocation, { rotation: { x: random(0, 360), y: random(0, 360) } });
-                            v.playSound("ldns.errormob_glitch", { volume: 0.1, pitch: 0.666 });
-                            await system.waitTicks(1);
-                        }
-                        v.kill();
-                        v.runCommand("stopsound @s");
-                    });
+                    nonebrain_despawn_events();
                     noneint = 0;
                 }
             }
+        }
+    });
+});
+
+function nonebrain_despawn_events() {
+    // 様々なイベントが起こる
+    world.getPlayers().forEach(async (v, i, a) => {
+        v.playSound("ldns.beep");
+        v.onScreenDisplay.setTitle("nonebrain");
+        await system.waitTicks(20 * 4);
+        const playerlocation = v.location;
+        const kanasibari = system.runInterval(async () => {
+            v.teleport(playerlocation);
+            v.playSound("ldns.errormob_glitch", { volume: 0.1, pitch: 0.666 });
+            const { container } = v.getComponent("inventory");
+            for (let i = 0; i < container.size; i++) {
+                container.swapItems(i, random(0, container.size - 1), container);
+            }
+            v.selectedSlotIndex = random(0, 9);
+            await system.waitTicks(1);
+        });
+        const rands = random(0, 2);
+        const randh = random(0, 4);
+        switch (rands) {
+            case 0:
+                v.playSound("ldns.pp_spawn");
+                v.playSound("ldns.beep");
+                v.playSound("ldns.errormob_glitch");
+                break;
+            case 1:
+                v.playSound("ldns.yy_spawn");
+                v.playSound("ldns.beep");
+                v.playSound("ldns.errormob_glitch");
+                break;
+        }
+        switch (randh) {
+            case 0:
+                v.onScreenDisplay.setTitle("egn2");
+                break;
+            case 1:
+                v.onScreenDisplay.setTitle("egn3");
+                break;
+            case 2:
+                v.onScreenDisplay.setTitle("egn6");
+                break;
+            case 3:
+                v.onScreenDisplay.setTitle("egn9");
+                break;
+        }
+        await system.waitTicks(20 * 4);
+        const randw = random(0, 2);
+        v.playSound("ldns.errormob_errorwindow");
+        switch (randw) {
+            case 0:
+                v.onScreenDisplay.setTitle("ew1");
+                break;
+            case 1:
+                v.onScreenDisplay.setTitle("ew2");
+                break;
+        }
+        await system.waitTicks(20 * 4);
+        v.playSound("ldns.time_mad");
+        console.info("We is cursed");
+        for (let i = 0; i < 160; i++) {
+            let titletextr = random(0, 6);
+            switch (titletextr) {
+                case 0:
+                    v.onScreenDisplay.setTitle("あなたは呪われてない", { stayDuration: 2, fadeInDuration: 0, fadeOutDuration: 0, subtitle: String(random(0, 999999999)) });
+                    break;
+                case 1:
+                    v.onScreenDisplay.setTitle("あなたは呪われている", { stayDuration: 2, fadeInDuration: 0, fadeOutDuration: 0, subtitle: String(random(0, 999999999)) });
+                    break;
+                case 2:
+                    v.onScreenDisplay.setTitle("縺ゅ↑縺溷測繧上ｌ縺ｦ", { stayDuration: 2, fadeInDuration: 0, fadeOutDuration: 0, subtitle: String(random(0, 999999999)) });
+                    break;
+                case 3:
+                    v.onScreenDisplay.setTitle("縺ゅ↑縺溷測繧上ｌ縺ｾ", { stayDuration: 2, fadeInDuration: 0, fadeOutDuration: 0, subtitle: String(random(0, 999999999)) });
+                    break;
+                case 4:
+                    v.onScreenDisplay.setTitle("We is Cursed.", { stayDuration: 2, fadeInDuration: 0, fadeOutDuration: 0, subtitle: String(random(0, 999999999)) });
+                    break;
+                case 5:
+                    v.onScreenDisplay.setTitle("They am Cursed.", { stayDuration: 2, fadeInDuration: 0, fadeOutDuration: 0, subtitle: String(random(0, 999999999)) });
+                    break;
+            }
+            world.setTimeOfDay(random(1, 23999));
+            await system.waitTicks(1);
+        }
+        const rande = random(0, 4);
+        if (rande === 0) {
+            v.playSound("ldns.beep");
+            v.onScreenDisplay.setTitle("Error1");
+        }
+        else if (rande === 1) {
+            v.playSound("ldns.beep");
+            v.onScreenDisplay.setTitle("Error2");
+        }
+        else if (rande === 2) {
+            v.playSound("ldns.beep");
+            v.onScreenDisplay.setTitle("Cursed1");
+        }
+        else if (rande === 3) {
+            v.playSound("ldns.beep");
+            v.onScreenDisplay.setTitle("Cursed2");
+        }
+        await system.waitTicks(20 * 4);
+        v.playSound("ldns.pp_spawn");
+        v.playSound("ldns.yy_spawn");
+        v.playSound("ldns.ppyy_spawn");
+        v.onScreenDisplay.setTitle("ppse");
+        await system.waitTicks(20 * 6);
+        system.clearRun(kanasibari);
+        for (i = 0; i < 120; i++) {
+            v.teleport(playerlocation, { rotation: { x: random(0, 360), y: random(0, 360) } });
+            v.playSound("ldns.errormob_glitch", { volume: 0.1, pitch: 0.666 });
+            await system.waitTicks(1);
+        }
+        v.kill();
+        v.runCommand("stopsound @s");
+    });
+}
+
+// エンティティが死亡したとき
+world.afterEvents.entityDie.subscribe(async (events) => {
+    system.run(async () => {
+        const { damageSource, deadEntity } = events;
+        if (!(damageSource.damagingEntity instanceof Player)) return;
+        if (deadEntity.typeId === "ldns:nonebrain") {
+            const items = damageSource.damagingEntity.runCommand('testfor @s[hasitem={item=ldns:pendant_of_twilight}]').successCount;
+            if (items >= 1 && damageSource.damagingEntity.hasTag("nonebrainchats")) {
+                noneint = 0;
+                damageSource.damagingEntity.playSound("random.totem", { pitch: 0.5, volume: 1.5 });
+                damageSource.damagingEntity.removeTag("nonebrainchats");
+            }
+        }
+    });
+});
+
+system.runInterval(async () => {
+    nonebrainchat = String(random(10000000, 99999999));
+    random_message_int = random(0, random_message.length);
+}, 20 * 60);
+
+world.beforeEvents.chatSend.subscribe(async (events) => {
+    system.run(async () => {
+        if (events.message === random_message[random_message_int]) {
+            world.sendMessage(nonebrainchat);
+        }
+        if (events.message === nonebrainchat) {
+            world.sendMessage("Nonebrains: " + String(noneint));
+            events.sender.addTag("nonebrainchats");
+            await system.waitTicks(20 * 60);
+            if (events.sender.hasTag("nonebrainchats")) {
+                nonebrain_despawn_events();
+            }
+            events.sender.removeTag("nonebrainchats");
         }
     });
 });
